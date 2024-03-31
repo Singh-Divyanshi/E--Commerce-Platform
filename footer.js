07a8194e6f1e15834269e4192f66316487127ef8
Divyanshi Singh <143312117+Singh-Divyanshi@users.noreply.github.com>
2024-03-31 07:29:28 +0530

    Add files via upload
    Our mission is to empower local artisans by providing them with a digital platform to showcase and sell their products globally.

---
+++ footer.js
@@ -0,0 +1,42 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+        <img src="img/light-logo.png" class="logo" alt="">
+        <div class="footer-ul-container">
+            <ul class="category">
+            <li class="category-title">Items</li>
+            <li><a href="#" class="footer-link">Pottery</a></li>
+            <li><a href="#" class="footer-link">Crockery</a></li>
+            <li><a href="#" class="footer-link">Platter</a></li>
+            <li><a href="#" class="footer-link">Paintings</a></li>
+            <li><a href="#" class="footer-link">Carpets</a></li>
+            <li><a href="#" class="footer-link">Puppets</a></li>
+            <li><a href="#" class="footer-link">Metal Work</a></li>
+            <li><a href="#" class="footer-link">Jodhpuri Juttis</a></li>
+            <li><a href="#" class="footer-link">Fabric Work</a></li>
+            </ul>
+            
+        </div>
+    </div>
+    <p class="footer-title">about company</p>
+    <p class="info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat tempore ad suscipit, eos eius quisquam sed optio nisi quaerat fugiat ratione et vero maxime praesentium, architecto minima reiciendis iste quo deserunt assumenda alias ducimus. Ullam odit maxime id voluptates rerum tenetur corporis laboriosam! Cum error ipsum laborum tempore in rerum necessitatibus nostrum nobis modi! Debitis adipisci illum nemo aperiam sed, et accusamus ut officiis. Laborum illo exercitationem quo culpa reprehenderit excepturi distinctio tempore cupiditate praesentium nisi ut iusto, assumenda perferendis facilis voluptas autem fuga sunt ab debitis voluptatum harum eum. Asperiores, natus! Est deserunt incidunt quasi placeat omnis, itaque harum?</p>
+    <p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
+    <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">terms & services</a>
+            <a href="#" class="social-link">privacy page</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+        </div>
+    </div>
+    <p class="footer-credit">Clothing, Best apparels online store</p>
+    `;
+}
+
+createFooter();
